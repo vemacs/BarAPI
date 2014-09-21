bae0e9e6e4cbcf7e513739b1ee30b1cd4ceb9bdc
vemacs <gentoo@outlook.com>
2014-09-21 08:18:50 -0600

    Add Spigot's falling object position fix to constructor

--- src/me/confuser/barapi/nms/v1_7.java
+++ src/me/confuser/barapi/nms/v1_7.java
@@ -139,14 +139,14 @@ public class v1_7 extends FakeDragon {
         
         try {
             packet = PacketPlayOutEntityTeleport.getConstructor(new Class<?>[] {
-                    int.class, int.class, int.class, int.class, byte.class, byte.class, boolean.class })
+                    int.class, int.class, int.class, int.class, byte.class, byte.class, boolean.class, boolean.class })
                     .newInstance(this.id, 
                             loc.getBlockX() * 32,
                             loc.getBlockY() * 32,
                             loc.getBlockZ() * 32,
                             (byte) ((int) loc.getYaw() * 256 / 360),
                             (byte) ((int) loc.getPitch() * 256 / 360),
-                            (boolean)false);
+                            false, false);
             
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
